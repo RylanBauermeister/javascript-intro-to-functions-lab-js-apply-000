2d4daac09df81bf81ed4ccc6ce2b9cd3c90cc134
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-01 19:36:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   else if(string === string.toLowerCase()){
     return "I can't hear you!"
   } else if(string === string.toUpperCase()){
-    return "YES, INDEED!"
+    return "YES INDEED!"
   } else {
     return "Grandma doesn't seem to notice you."
   }
